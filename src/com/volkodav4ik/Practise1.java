4c09b7bfaa5fc9e08ce3743aa19e3b00f1e60158
Artem Vovk <volkodav4ik1990@gmail.com>
2019-11-30 13:45:29 +0200

    Add first task

---
+++ src/com/volkodav4ik/Practise1.java
@@ -0,0 +1,23 @@
+package com.volkodav4ik;
+
+public class Practise1 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 99; i++) {
+            if ((i % 3) == 0 && (i % 5) == 0) {
+                System.out.print("HelloWorld ");
+            } else {
+                if ((i % 3) == 0) {
+                    System.out.print("Hello ");
+                } else {
+                    if ((i % 5) == 0) {
+                        System.out.print("World");
+                    } else {
+                        System.out.print(i + " ");
+                    }
+                }
+            }
+
+        }
+    }
+}
